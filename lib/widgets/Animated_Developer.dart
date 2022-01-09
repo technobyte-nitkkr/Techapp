0a14f421f5b57a8e665a615bdfc623d7c443cd4e
Chetan <61468014+Unknown39825@users.noreply.github.com>
2022-01-09 21:26:51 +0530

    developer card

--- lib/widgets/Animated_Developer.dart
+++ lib/widgets/Animated_Developer.dart
@@ -1,6 +1,7 @@
 // @dart=2.9
 import 'dart:async';
 import 'dart:ui';
+import 'package:card_swiper/card_swiper.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:loading_animation_widget/loading_animation_widget.dart';
@@ -18,44 +19,11 @@ class AnimatedDeveloper extends StatefulWidget {
 }
 
 class _AnimatedDeveloperState extends State<AnimatedDeveloper> {
-  List<String> data = [
-    "https://cdn.dribbble.com/users/3281732/screenshots/8159457/media/9e7bfb83b0bd704e941baa7a44282b22.jpg?compress=1&resize=600x600",
-    "https://cdn.dribbble.com/users/3281732/screenshots/7012328/media/bcd672685071ca4da27d5f3ea44ac5db.jpg?compress=1&resize=600x600",
-    "https://cdn.dribbble.com/users/3281732/screenshots/6727912/samji_illustrator.jpeg?compress=1&resize=600x600",
-    "https://cdn.dribbble.com/users/3281732/screenshots/10940512/media/b2a8ea95c550e5f09d0ca07682a3c0da.jpg?compress=1&resize=600x600",
-    "https://cdn.dribbble.com/users/3281732/screenshots/8616916/media/a7e39b15640f8883212421d134013e38.jpg?compress=1&resize=600x600",
-    "https://cdn.dribbble.com/users/3281732/screenshots/6590709/samji_illustrator.jpg?compress=1&resize=600x600",
-  ];
-
   int _currentPage = 0;
-  PageController _pageController = PageController(
-    initialPage: 0,
-  );
-  Timer _timer;
-
-  @override
-  void initState() {
-    super.initState();
-    _currentPage = 0;
-    _timer = Timer.periodic(Duration(seconds: 2), (Timer timer) {
-      if (_currentPage < 5) {
-        _currentPage++;
-      } else {
-        _currentPage = 0;
-      }
-
-      _pageController.animateToPage(
-        _currentPage,
-        duration: Duration(milliseconds: 350),
-        curve: Curves.easeIn,
-      );
-    });
-  }
 
   @override
   void dispose() {
     super.dispose();
-    _timer?.cancel();
   }
 
   @override
@@ -90,17 +58,20 @@ class _AnimatedDeveloperState extends State<AnimatedDeveloper> {
               AnimatedSwitcher(
                 duration: Duration(milliseconds: 500),
                 child: Container(
-                  key: ValueKey<String>(data[_currentPage]),
+                  height: MediaQuery.of(context).size.height,
+                  width: MediaQuery.of(context).size.width,
                   decoration: BoxDecoration(
                     image: DecorationImage(
-                      image: NetworkImage(data[_currentPage]),
+                      image: AssetImage('assets/images/developers/wall' +
+                          (_currentPage + 1).toString() +
+                          '.png'),
                       fit: BoxFit.cover,
                     ),
                   ),
                   child: BackdropFilter(
                     filter: ImageFilter.blur(
-                      sigmaX: 15,
-                      sigmaY: 15,
+                      sigmaX: 5,
+                      sigmaY: 5,
                     ),
                     child: Container(
                       color: Colors.black.withOpacity(0.2),
@@ -108,23 +79,35 @@ class _AnimatedDeveloperState extends State<AnimatedDeveloper> {
                   ),
                 ),
               ),
-              FractionallySizedBox(
-                heightFactor: 0.55,
-                child: PageView.builder(
-                  itemCount: developers.length,
-                  onPageChanged: (int page) {
-                    setState(() {
-                      _currentPage = page;
-                    });
-                  },
-                  controller: _pageController,
-                  itemBuilder: (BuildContext context, int index) {
-                    return FractionallySizedBox(
-                        widthFactor: 0.8,
-                        child: DeveloperWidget(developer: developers[index]));
-                  },
-                ),
-              )
+              Column(
+                children: [
+                  SizedBox(
+                    height: 100,
+                  ),
+                  Container(
+                    height: MediaQuery.of(context).size.height * 0.7,
+                    padding: EdgeInsets.symmetric(horizontal: 20),
+                    child: Swiper(
+                      itemHeight: MediaQuery.of(context).size.height * 0.5,
+                      itemWidth: MediaQuery.of(context).size.width * 0.8,
+                      onIndexChanged: (index) {
+                        setState(() {
+                          _currentPage = index % 6;
+                        });
+                      },
+                      itemCount: developers.length,
+                      layout: SwiperLayout.TINDER,
+                      autoplay: true,
+                      autoplayDelay: 2000,
+                      itemBuilder: (context, index) {
+                        return DeveloperWidget(
+                          developer: developers[index],
+                        );
+                      },
+                    ),
+                  ),
+                ],
+              ),
             ],
           );
         } else {
--- lib/widgets/developer_card.dart
+++ lib/widgets/developer_card.dart
@@ -1,6 +1,5 @@
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
-import 'package:techapp/screens/components/style.dart';
 
 class DeveloperCard extends StatelessWidget {
   final String imageSrc;
@@ -22,55 +21,43 @@ class DeveloperCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      // padding: EdgeInsets.all(20),
       decoration: BoxDecoration(
         color: Colors.white,
         borderRadius: BorderRadius.circular(13),
-        boxShadow: [
-          BoxShadow(
-            offset: Offset(0, 17),
-            blurRadius: 17,
-            spreadRadius: -23,
-            color: kShadowColor,
-          ),
-        ],
       ),
-      child: Material(
-        color: Colors.transparent,
-        child: InkWell(
-          onTap: press,
-          child: Padding(
-            padding: const EdgeInsets.all(10.0),
-            child: Column(
-              children: <Widget>[
-                Spacer(),
-                Image.network(
-                  imageSrc,
-                  height: 180,
-                  width: 180,
-                  fit: BoxFit.cover,
-                ),
-                Divider(),
-                AutoSizeText(
-                  name,
-                  style: TextStyle(
-                    fontSize: 25.0,
-                  ),
-                  maxLines: 1,
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                AutoSizeText(
-                  year + ' Year',
-                  style: TextStyle(
-                    fontSize: 25.0,
-                  ),
-                  maxLines: 1,
-                ),
-              ],
+      child: InkWell(
+        onTap: press,
+        child: Column(
+          children: <Widget>[
+            Spacer(),
+            Image.network(
+              imageSrc,
+              height: 180,
+              width: 180,
+              fit: BoxFit.cover,
             ),
-          ),
+            Divider(),
+            AutoSizeText(
+              name,
+              style: TextStyle(
+                fontSize: 25.0,
+              ),
+              maxLines: 1,
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            AutoSizeText(
+              year + ' Year',
+              style: TextStyle(
+                fontSize: 25.0,
+              ),
+              maxLines: 1,
+            ),
+            SizedBox(
+              height: 10,
+            ),
+          ],
         ),
       ),
     );
